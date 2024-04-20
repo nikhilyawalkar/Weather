06168c8a91806f4f3fcca0a480f70c59b63245e7
Nik <yawalkar.nikhil@gmail.com>
2024-04-20 10:25:13 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Nikhil yawalkar on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
